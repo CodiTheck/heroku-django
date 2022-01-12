23e47a5f3ebec628d684b18ebaf55d279a1c20f3
Dr Mokira <arnoldmokira3d48@gmail.com>
2022-01-12 10:53:59 -0900

    ioc program is updated.

--- stdlib/ioc.py
+++ stdlib/ioc.py
@@ -1,9 +1,12 @@
 import logging
 
+
 logger_format = "[%(asctime)s %(msecs)03dms] [PID %(process)d %(threadName)s] %(message)s";
 logging.basicConfig(format=logger_format, level=logging.INFO, datefmt="%Y-%m-%d %I:%M:%S %p %Z%z");
 
+
 def log(message):
+    """ Function that is used to print the logs. """
     logging.info(message);
 
 
