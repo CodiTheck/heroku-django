a72c0d1cd92e14575dbed243b19773aff35d3890
Dr Mokira <arnoldmokira3d48@gmail.com>
2022-02-02 12:38:05 -0900

    API of JWT Authentication system is added.

--- core/urls.py
+++ core/urls.py
@@ -18,10 +18,13 @@ from django.urls import path
 
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('api/auth/refresh/', jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
 ];
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT);
