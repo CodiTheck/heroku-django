0c404f49d331186408b1eaf3d3777a04e4568e50
Dr Mokira <arnoldmokira3d48@gamil.com>
2022-07-20 16:55:41 -0800

    Virtual env function is updated.

--- stdlib/env.py
+++ stdlib/env.py
@@ -3,13 +3,9 @@ import os
 def get_env_var(var_name, default_value=None):
     """ Program that is used to recovery the environment data """
     try:
-        return os.environ[var_name];
-    except KeyError:
-        if default_value is None:
-            error_msg = "Set the {} environment variable".format(var_name);
-            raise ValueError("No key and no default value!");
-        else:
-            return default_value;
+        return os.environ.get(var_name, default_value);
+    except:
+        return None;
 
 
 pass;
